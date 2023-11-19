7e19382433c2746d17b9a2bfcd5a7d08eba02a78
tai.nguyen <tai.nguyen24816@gmail.com>
2023-11-19 23:41:27 +0700

    Update Led task

--- OS_Task/OS_Task.c
+++ OS_Task/OS_Task.c
@@ -2,6 +2,7 @@
 #include "task.h"
 #include "OS_Task.h"
 #include <stdio.h>
+#include "stm32f4xx_hal.h"
 
 void Led_1_Handler(void * Task_Param)
 {
@@ -9,22 +10,34 @@ void Led_1_Handler(void * Task_Param)
     /* Infinitive loop */
     for(;;)
     {
-        // snprintf(data, 100, "%s\n", (char *)Task_Param);
-        // SEGGER_SYSVIEW_PrintfTarget(data);
-        
-        
-
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_10);
+        SEGGER_SYSVIEW_PrintfTarget("Task_1");
+        HAL_Delay(1000);
         taskYIELD();
     };
-}
+    }
 
 void Led_2_Handler(void * Task_Param)
 {
     /* Infinitive loop */
     for(;;)
     {
-        // snprintf(data, 100, "%s\n", (char *)Task_Param);
-        // SEGGER_SYSVIEW_PrintfTarget(data);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_12);
+        SEGGER_SYSVIEW_PrintfTarget("Task_2");
+        HAL_Delay(800);
+        taskYIELD();
+    };
+}
+
+void Led_3_Handler(void * Task_Param)
+{
+
+    /* Infinitive loop */
+    for(;;)
+    {
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        SEGGER_SYSVIEW_PrintfTarget("Task_3");
+        HAL_Delay(400);
         taskYIELD();
     };
 }
