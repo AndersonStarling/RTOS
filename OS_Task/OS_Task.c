afb7a3e925948366bd5b205d629c56c0e7bff672
tai.nguyen <tai.nguyen24816@gmail.com>
2023-11-22 22:18:34 +0700

    Update notify and shutdown task

--- OS_Task/OS_Task.c
+++ OS_Task/OS_Task.c
@@ -13,7 +13,7 @@ extern TaskHandle_t Task_Shutdown_Handler_Kernel_Pointer;
 
 void Led_1_Handler(void * Task_Param)
 {
-    uint32_t pulNotificationValue;
+    uint32_t Button_Status;
     BaseType_t Status;
     uint32_t Task_ID = 0;
 
@@ -24,17 +24,17 @@ void Led_1_Handler(void * Task_Param)
         HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_10);
         vTaskDelay(pdMS_TO_TICKS(1000));
 
-        Status = xTaskNotifyWait( 0x0, 0xffffffff, &pulNotificationValue, pdMS_TO_TICKS(OS_TASK_TIME_OUT));
+        Status = xTaskNotifyWaitIndexed(0, 0x0, 0xffffffff, &Button_Status, pdMS_TO_TICKS(OS_TASK_TIME_OUT));
 
         /* A event came */
         if(Status == pdTRUE)
         {
-            switch(pulNotificationValue)
+            switch(Button_Status)
             {
                 case 0:
+                    Status = xTaskNotifyIndexed(Task_Shutdown_Handler_Kernel_Pointer, 1, Task_ID, eSetValueWithOverwrite);
                     break;
                 case 1:
-                    xTaskNotify(Task_Shutdown_Handler_Kernel_Pointer, Task_ID, eNoAction);
                 	break;
                 default:
                 	break;
@@ -45,17 +45,41 @@ void Led_1_Handler(void * Task_Param)
 
 void Led_2_Handler(void * Task_Param)
 {
+    uint32_t Task_ID = 1;
+    uint32_t Button_Status;
+    BaseType_t Status;
+
     /* Infinitive loop */
     for(;;)
     {
         SEGGER_SYSVIEW_PrintfTarget("Task_2");
         HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_12);
         vTaskDelay(pdMS_TO_TICKS(800));
+
+        Status = xTaskNotifyWaitIndexed(0, 0x0, 0xffffffff, &Button_Status, pdMS_TO_TICKS(OS_TASK_TIME_OUT));
+
+        /* A event came */
+        if(Status == pdTRUE)
+        {
+            switch(Button_Status)
+            {
+                case 0:
+                    Status = xTaskNotifyIndexed(Task_Shutdown_Handler_Kernel_Pointer, 1, Task_ID, eSetValueWithOverwrite);
+                    break;
+                case 1:
+                	break;
+                default:
+                	break;
+            }
+        }
     };
 }
 
 void Led_3_Handler(void * Task_Param)
 {
+    uint32_t Task_ID = 2;
+    uint32_t Button_Status;
+    BaseType_t Status;
 
     /* Infinitive loop */
     for(;;)
@@ -63,45 +87,71 @@ void Led_3_Handler(void * Task_Param)
         SEGGER_SYSVIEW_PrintfTarget("Task_3");
         HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
         vTaskDelay(pdMS_TO_TICKS(400));
+
+        Status = xTaskNotifyWaitIndexed(0, 0x0, 0xffffffff, &Button_Status, pdMS_TO_TICKS(OS_TASK_TIME_OUT));
+
+        /* A event came */
+        if(Status == pdTRUE)
+        {
+            switch(Button_Status)
+            {
+                case 0:
+                    Status = xTaskNotifyIndexed(Task_Shutdown_Handler_Kernel_Pointer, 1, Task_ID, eSetValueWithOverwrite);
+                    break;
+                case 1:
+                	break;
+                default:
+                	break;
+            }
+        }
     };
 }
 
 void User_Button_Handler(void * Task_Param)
 {
     GPIO_PinState Button_Status;
+    TickType_t xLastWakeTime;
+
+    xLastWakeTime = xTaskGetTickCount();
+
     for(;;)
     {
         SEGGER_SYSVIEW_PrintfTarget("Task_4_User_Button");
-        vTaskDelay(pdMS_TO_TICKS(10));
+        xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1));
         Button_Status = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
-        xTaskNotify(Led_1_Handler_Kernel_Pointer, Button_Status, eSetValueWithOverwrite);
+        xTaskNotifyIndexed(Led_1_Handler_Kernel_Pointer, 0, Button_Status, eSetValueWithOverwrite);
+        xTaskNotifyIndexed(Led_2_Handler_Kernel_Pointer, 0, Button_Status, eSetValueWithOverwrite);
+        xTaskNotifyIndexed(Led_3_Handler_Kernel_Pointer, 0, Button_Status, eSetValueWithOverwrite);
     }
 }
 
 void Task_Shutdown_Handler(void * Task_Param)
 {
-    uint32_t pulNotificationValue;
+    uint32_t Task_ID;
     BaseType_t Status;
 
     /* Infinitive loop */
     for(;;)
     {
         SEGGER_SYSVIEW_PrintfTarget("Task_Shut_Down");
-        Status = xTaskNotifyWait( 0x0, 0xffffffff, &pulNotificationValue, pdMS_TO_TICKS(OS_TASK_TIME_OUT));
+        Status = xTaskNotifyWaitIndexed(1, 0x0, 0xffffffff, &Task_ID, pdMS_TO_TICKS(OS_TASK_TIME_OUT));
 
         /* A event came */
         if(Status == pdTRUE)
         {
-            switch(pulNotificationValue)
+            switch(Task_ID)
             {
                 case 0:
                     vTaskDelete(Led_1_Handler_Kernel_Pointer);
+                    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_RESET);
                     break;
                 case 1:
                     vTaskDelete(Led_2_Handler_Kernel_Pointer);
+                    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_RESET);
                 	break;
                 case 2:
                     vTaskDelete(Led_3_Handler_Kernel_Pointer);
+                    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
                     break;
                 default:
                 	break;
