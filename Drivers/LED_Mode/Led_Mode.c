28b3c57034c04ca7961b82e563dac1935dd8c970
tai.nguyen <tai.nguyen24816@gmail.com>
2023-12-03 21:18:33 +0700

    Update led mode

--- Drivers/LED_Mode/Led_Mode.c
+++ Drivers/LED_Mode/Led_Mode.c
@@ -1,20 +1,22 @@
 #include "Led_Mode.h"
 #include "stm32f4xx_hal.h"
 
-
 void LED_Mode_0(void)
 {
-    
+    HAL_GPIO_TogglePin(PORT_LED_0, PIN_LED_0);
+    HAL_GPIO_TogglePin(PORT_LED_1, PIN_LED_1);
+    HAL_GPIO_TogglePin(PORT_LED_2, PIN_LED_2);
 }
 
 void LED_Mode_1(void)
 {
-    
+    HAL_GPIO_TogglePin(PORT_LED_0, PIN_LED_0);
+    HAL_GPIO_TogglePin(PORT_LED_1, PIN_LED_1);
 }
 
 void LED_Mode_2(void)
 {
-    
+    HAL_GPIO_TogglePin(PORT_LED_0, PIN_LED_0);
 }
 
 
