4987228faf461183be631084a9896b2de86ce002
tai.nguyen <tai.nguyen24816@gmail.com>
2023-12-03 21:05:49 +0700

    Update Led mode

---
+++ Drivers/LED_Mode/Led_Mode.c
@@ -0,0 +1,27 @@
+#include "Led_Mode.h"
+#include "stm32f4xx_hal.h"
+
+
+void LED_Mode_0(void)
+{
+    
+}
+
+void LED_Mode_1(void)
+{
+    
+}
+
+void LED_Mode_2(void)
+{
+    
+}
+
+
+
+
+
+
+
+
+
---
+++ Drivers/LED_Mode/Led_Mode.h
@@ -0,0 +1,25 @@
+#ifndef LED_MODE_H
+#define LED_MODE_H
+
+/* Led 0 */
+#define PORT_LED_0   GPIOC
+#define PIN_LED_0    GPIO_PIN_10
+
+/* Led 1 */
+#define PORT_LED_1   GPIOC
+#define PIN_LED_1    GPIO_PIN_12
+
+/* Led 2 */
+#define PORT_LED_2   GPIOB
+#define PIN_LED_2    GPIO_PIN_7
+
+#endif /* LED_MODE_H */
+
+
+
+
+
+
+
+
+
